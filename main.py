8539bd1b4a7616a9485b189c3936838cd50e9566
timvolzh <nigaman95@gmail.com>
2022-06-07 16:09:36 +0300

    no message

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import flask
-import sqlalchemy
 from flask import Flask, request
 from flask.views import MethodView
 
 from sqlalchemy import exc
 from sqlalchemy import Column, String, DateTime, Integer, create_engine, func
-from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
